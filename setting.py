701120152358ffc1aaae31d57d8aa001c34eb557
psb2199 <psb2199@naver.com>
2022-10-16 18:27:07 +0900

    .

--- setting.py
+++ setting.py
@@ -1,11 +1,13 @@
 from pico2d import *
 
-open_canvas(400,400)
+open_canvas(500,700)
 
 player = load_image('player(50x50).png')
 enemy = load_image('enemy(50x50).png')
 
-player.draw_now(200,0)
-enemy.draw_now(200,400)
+player.draw_now(250,50)
+enemy.draw_now(250,650)
+
+delay(10)
 
 close_canvas()
