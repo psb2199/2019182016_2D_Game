1885c6c29647b630f4365f9521bbbca224c8d253
psb2199 <psb2199@naver.com>
2022-10-17 02:35:49 +0900

    플레이어 움직임 구현

--- setting.py
+++ setting.py
@@ -1,19 +1,71 @@
 from pico2d import *
 
-open_canvas(500,700)
+open_canvas(400,600)
 
 player = load_image('player(50x50).png')
 enemy = load_image('enemy(50x50).png')
 
-player.draw_now(250,50)
-enemy.draw_now(250,650)
+def handle_events():
+    global running
+    global dir_x
+    global dir_y
 
-y = 0
-while (y<650):
-    clear_canvas_now()
-    player.draw_now(250,50)
-    enemy.draw_now(250,650-y)
-    y += 2
+    events = get_events()
+    for event in events:
+
+        if event.type == SDL_QUIT:
+            running = False
+
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_x += 1
+            elif event.key == SDLK_LEFT:
+                dir_x -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
+
+            elif event.key == SDLK_ESCAPE:
+                running = False
+
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x -= 1
+            elif event.key == SDLK_LEFT:
+                dir_x += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
+
+
+running = True
+
+x = 200
+y = 50
+dir_x = 0
+dir_y = 0
+
+while (running == True):
+    clear_canvas()
+    player.draw(x,y)
+    update_canvas()
+    handle_events()
+
+    x = x + (dir_x * 4)
+    y = y + (dir_y * 4)
+
+    if(x<15):
+        x = 15
+    elif(385<x):
+        x = 385
+    elif (y < 15):
+        y = 15
+    elif (585 < y):
+        y = 585
+
     delay(0.01)
+
 
 close_canvas()
